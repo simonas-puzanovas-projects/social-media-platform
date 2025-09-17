2ca1a9119d713522b1f48611966611ebcdb23f90
simonas-puzanovas-projects <spuzanovas1@gmail.com>
2025-09-17 22:29:27 +0300

    Add socket event handling and fix SQLAlchemy queries in chat system
    - Add socket.emit for real-time message delivery
    - Add socket event listener initialization with retry mechanism in chat.js
    - Fix SQLAlchemy OR/AND operators to use bitwise & and | operators
    - Update sender field to use username instead of user_id for proper message display
    - Move chat.js script loading to head for proper initialization order

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, session, json, request
 from ..helpers import get_friendship_status, find_friendship, create_notification, clean_notification_data, get_friends_query, get_user_messenger
 from ..models import Messenger, Message, Friendship, User
-from .. import db
+from .. import db, socketio
 
 bp_chat = Blueprint("bp_chat", __name__, template_folder="../templates")
 
@@ -31,8 +31,8 @@ def send_message():
     friend_id = db.session.query(User).filter(User.username == request.form.get("friend_username")).first().id
 
     messenger = db.session.query(Messenger).filter(
-        (current_user_id == Messenger.first_user_id) and (friend_id == Messenger.second_user_id) or
-        (friend_id == Messenger.first_user_id) and (current_user_id == Messenger.second_user_id)
+        (current_user_id == Messenger.first_user_id) & (friend_id == Messenger.second_user_id) |
+        (friend_id == Messenger.first_user_id) & (current_user_id == Messenger.second_user_id)
         ).first()
 
     new_message = Message(
@@ -46,12 +46,13 @@ def send_message():
 
     message_json = {
         'content': new_message.content,
-        'sender': session["user_id"]
+        'sender': db.session.query(User).get(session["user_id"]).username
     }
+
+    socketio.emit("new_message", message_json, room=f'user_{friend_id}')
 
     return render_template("partials/message.html", message = message_json)
 
-#TODO: figure this out tommorow idiot
 @bp_chat.route("/chat/open/<username>")
 def open_chat(username):
 
@@ -64,20 +65,28 @@ def open_chat(username):
             friend_id = friend.id
 
     messenger = db.session.query(Messenger).filter(
-        (current_user_id == Messenger.first_user_id) and (friend_id == Messenger.second_user_id) or
-        (friend_id == Messenger.first_user_id) and (current_user_id == Messenger.second_user_id)
+        ((current_user_id == Messenger.first_user_id) & (friend_id == Messenger.second_user_id)) |
+        ((friend_id == Messenger.first_user_id) & (current_user_id == Messenger.second_user_id))
         ).first()
     
     if messenger:
         messages = db.session.query(Message).filter(messenger.id == Message.messenger_id).all()
+        print("found")
 
         json_data = []
+
         for message in messages:
-            json_data.append({
-                "sender": db.session.query(User).filter(User.id == current_user_id).first().username,
-                "content": message.content
-            })
+            sender = db.session.query(User).filter(User.id == message.sender_id).first()
+
+            if sender:
+                json_data.append({
+                    "sender": sender.username,
+                    "content": message.content
+                })
+
+            else: print("friends name not found")
 
         return render_template("partials/chat_messenger_chat.html", username = username, messages = json_data)
+    print("not found")
 
 
--- app/static/chat.js
+++ app/static/chat.js
@@ -1,1 +1,32 @@
-var current_friend_chat_id = 0
+var current_chat_friend_name = null
+
+function createMessageHTML(message) {
+    const isCurrentUser = message.sender === current_chat_friend_name;
+    const color = isCurrentUser ? 'orange' : 'yellow';
+    const align = isCurrentUser ? 'right' : 'left';
+
+    return `
+        <div>
+            <h3 class="message-text" style="color: ${color}; text-align: ${align};">
+                ${message.sender}: ${message.content}
+            </h3>
+        </div>
+    `;
+}
+
+function init_chat_socket(){
+    if (typeof socket !== 'undefined') {
+        socket.on('new_message', function(data) {
+            
+            if (data.sender === current_chat_friend_name) {
+                const messageHTML = createMessageHTML(data);
+                document.getElementById('chat-messages').insertAdjacentHTML('beforeend', messageHTML);
+            }
+        });
+    }
+    else{
+        setTimeout(init_chat_socket, 50)
+    }
+}
+
+init_chat_socket()
